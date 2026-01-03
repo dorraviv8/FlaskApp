613ea98edf739271cba4d04e875284053d31b709
Dor Raviv <dorraviv8@gmail.com>
2026-01-03 17:30:36 +0200

    adding from flask import jsonify to app.py

--- app/app.py
+++ app/app.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from flask import jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+@app.route("/health", methods=["GET"])
+def health():
+    return jsonify(status="ok"), 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
